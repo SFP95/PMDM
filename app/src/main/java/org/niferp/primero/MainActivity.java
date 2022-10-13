5295a807a64b18980c780b070a9538202d0a95bf
Nia <103027252+SFP95@users.noreply.github.com>
2022-10-13 09:50:14 +0200

    añadido texto y  boton a la app

--- app/src/main/java/org/niferp/primero/MainActivity.java
+++ app/src/main/java/org/niferp/primero/MainActivity.java
@@ -1,17 +1,33 @@
 package org.niferp.primero;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-import android.util.Log;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        Log.d("NIA","Mi  primer mensaje de Logat");
-        setContentView(R.layout.activity_main);
+      //  super.onCreate(savedInstanceState);
+
+            super.onCreate(savedInstanceState);
+
+            ViewGroup vg = new LinearLayout(this);
+
+            Button b =new Button(this);
+            b.setText("PULSA");
+            TextView tv =new TextView(this);
+            tv.setText("TEXTO");
+
+            vg.addView(b);
+            vg.addView(tv);
+
+            setContentView(vg);
+        //Log.d("NIA", "Mi  primer mensaje de Logat");
+        //setContentView(R.layout.activity_main);
 
 
     }
