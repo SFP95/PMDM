f30934dfa81cedd3d0a358761beab3c7969183e1
Nia <103027252+SFP95@users.noreply.github.com>
2022-10-20 10:03:26 +0200

    cambio de texto desde java a boton  desde xml

--- app/src/main/java/org/niferp/primero/MainActivity.java
+++ app/src/main/java/org/niferp/primero/MainActivity.java
@@ -2,6 +2,7 @@ package org.niferp.primero;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 
@@ -14,10 +15,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button  b=(Button) findViewById(R.id.idBoton);
-        b.setText("CAMBIATO DESDE JAVA");
 
+        //String antiguaEtiqueta=b.getText().toString();
+        //b.setText(antiguaEtiqueta  += "FUERTE");
 
-
+       /* b.setOnClickListener(new View.OnClickListener() {
+                                 @Override
+                                 public void onClick(View view) {
+                                    accion(view);
+                                 }
+                             }
+        );*/
     }
 
+    private void accion(View view){
+        Button boton = (Button) view;
+        boton.setText("PULSADO");
+    }
 }
