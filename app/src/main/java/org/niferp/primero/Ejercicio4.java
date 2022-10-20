192108a2432b7016f98a09607ee6a88ffb51126e
Nia <103027252+SFP95@users.noreply.github.com>
2022-10-20 10:35:48 +0200

    creacion de ejercicio 4 hasta apartado 8

---
+++ app/src/main/java/org/niferp/primero/Ejercicio4.java
@@ -0,0 +1,23 @@
+package org.niferp.primero;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.widget.Button;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Ejercicio4 extends AppCompatActivity {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button b=(Button) findViewById(R.id.idBoton);
+    b.setOnClickListener(v -> accion());
+    }
+
+    private void accion() {
+        Button b=(Button) findViewById(R.id.idBoton);
+        b.setText("DOS");
+    }
+}
