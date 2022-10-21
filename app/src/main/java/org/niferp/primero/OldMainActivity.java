3a447c225e92ceccdee025b2894193e9a8450133
Nia <103027252+SFP95@users.noreply.github.com>
2022-10-21 10:31:11 +0200

    nueva clase

---
+++ app/src/main/java/org/niferp/primero/OldMainActivity.java
@@ -0,0 +1,38 @@
+package org.niferp.primero;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+
+public class OldMainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button  b=(Button) findViewById(R.id.idBoton);
+
+        //String antiguaEtiqueta=b.getText().toString();
+        //b.setText(antiguaEtiqueta  += "FUERTE");
+
+       /* b.setOnClickListener(new View.OnClickListener() {
+                                 @Override
+                                 public void onClick(View view) {
+                                    accion(view);
+                                 }
+                             }
+        );*/
+        //b.setOnClickListener(v ->accion(v)); //es lo mismo que lo de arriba pero usando el metodo lambda
+        b.setOnClickListener(v ->accion());
+    }
+    //private void accion(View view)
+    private void accion(){
+        //Button boton = (Button) view;
+        Button boton = (Button) (findViewById(R.id.idBoton));
+        boton.setText("PULSADO");
+    }
+}
