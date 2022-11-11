eb8b9fbb4457a9c977eb43448f93b2ad99f6de6a
Nia <103027252+SFP95@users.noreply.github.com>
2022-11-11 10:27:26 +0100

    clase 11/11/22

--- app/src/main/java/org/niferp/primero/OldMainActivity.java
+++ app/src/main/java/org/niferp/primero/OldMainActivity.java
@@ -108,3 +108,92 @@ tv.setText(mensaje);
 }
 
  */
+
+/** ACT 4
+ * package org.niferp.primero;
+ *
+ * import androidx.appcompat.app.AppCompatActivity;
+ *
+ * import android.content.Context;
+ * import android.content.res.Resources;
+ * import android.os.Bundle;
+ * import android.view.View;
+ * import android.widget.Button;
+ * import android.widget.CheckBox;
+ * import android.widget.RadioButton;
+ * import android.widget.RadioGroup;
+ * import android.widget.TextView;
+ * import android.widget.Toast;
+ *
+ *
+ * public class MainActivity extends AppCompatActivity {
+ *
+ *     private RadioGroup menu;
+ *     private RadioButton rbLight;
+ *     private RadioButton rbNormal;
+ *     private RadioButton rbGraso;
+ *     private CheckBox ckBrocoli;
+ *     private CheckBox ckJamonYork;
+ *     private CheckBox ckHamburguesa;
+ *     private CheckBox ckTorreznos;
+ *     private TextView banner;
+ *
+ *     @Override
+ *     protected void onCreate(Bundle savedInstanceState) {
+ *
+ *         super.onCreate(savedInstanceState);
+ *         setContentView(R.layout.activity_main);
+ *         asociarControles();
+ *         configurarListeners();
+ *
+ *     }
+ *
+ *     private void asociarControles() {
+ *         this.menu=findViewById(R.id.radioGroup);
+ *         this.rbLight=findViewById(R.id.rbLight);
+ *         this.rbNormal=findViewById(R.id.rbNormal);
+ *         this.rbGraso=findViewById(R.id.rbGraso);
+ *         this.ckBrocoli=findViewById(R.id.ckBrocoli);
+ *         this.ckJamonYork=findViewById(R.id.ckJamonYork);
+ *         this.ckHamburguesa=findViewById(R.id.ckHamburguesa);
+ *         this.ckTorreznos=findViewById(R.id.ckTorreznos);
+ *         this.banner=findViewById(R.id.banner);
+ *     }
+ *     private void configurarListeners() {
+ *         this.menu.setOnCheckedChangeListener((cb,b)->actualizar());
+ *         this.ckBrocoli.setOnCheckedChangeListener((cb,b)->actualizar());
+ *         this.ckJamonYork.setOnCheckedChangeListener((cb,b)->actualizar());
+ *         this.ckHamburguesa.setOnCheckedChangeListener((cb,b)->actualizar());
+ *         this.ckTorreznos.setOnCheckedChangeListener((cb,b)->actualizar());
+ *     }
+ *     private  void actualizar(){
+ *         int calorias=0;
+ *         calorias += ckBrocoli.isChecked()? 100:0;
+ *         calorias += ckJamonYork.isChecked()? 500:0;
+ *         calorias += ckHamburguesa.isChecked()? 1500:0;
+ *         calorias += ckTorreznos.isChecked()? 2000:0;
+ *
+ *         int min=0;
+ *         int max=0;
+ *
+ *        switch (menu.getCheckedRadioButtonId()){
+ *            case R.id.rbLight:min=0;max=1000;break;
+ *            case R.id.rbNormal:min=1000;max=2000 ; break;
+ *            case R.id.rbGraso:min=2000;max=100000; break;
+ *
+ *        }
+ *         String mensaje=(calorias >= min && calorias < max)? "OK" : "FATAL";
+ *         mensaje += " - Te estás enchufando %1$d calorías";
+ *         mensaje= String.format(mensaje, calorias);
+ *
+ *         this.banner.setText(mensaje);
+ *     }
+ *     private void tostada(String mensaje){ //ventanita emergente que dura unos segundos
+ *         Context context= getApplicationContext();
+ *         int duracion= Toast.LENGTH_SHORT;
+ *         Toast toast= Toast.makeText(context,mensaje,duracion);
+ *         toast.show();
+ *     }
+ *     //version lambda
+ * }
+ */
