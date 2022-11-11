e965a810e82a849d03422b0642dd117953381962
Nia <103027252+SFP95@users.noreply.github.com>
2022-11-11 10:38:29 +0100

    clase 11/11/22

---
+++ app/src/main/java/org/niferp/primero/SecundaryActivity.java
@@ -0,0 +1,14 @@
+package org.niferp.primero;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SecundaryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
