03cfc77e66fa73f3a5a4ee1ff1d9e54876e3de09
Nia <103027252+SFP95@users.noreply.github.com>
2022-11-11 10:52:00 +0100

    clase 11/11/22

--- app/src/main/java/org/niferp/primero/MainActivity.java
+++ app/src/main/java/org/niferp/primero/MainActivity.java
@@ -3,6 +3,7 @@ package org.niferp.primero;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.View;
@@ -22,6 +23,14 @@ public class MainActivity extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+    public void pulsar(View view){
+        //Forma de arrancar la ventana seucndaria 5.0
+        startActivity(new Intent (this,SecundaryActivity.class));
+        Toast.makeText(this,"SORPRESA",Toast.LENGTH_SHORT).show();
+
+        //5.1
 
     }
 
--- app/src/main/java/org/niferp/primero/SecundaryActivity.java
+++ app/src/main/java/org/niferp/primero/SecundaryActivity.java
@@ -4,11 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class SecundaryActivity extends AppCompatActivity {
+public class SecundaryActivity extends AppCompatActivity{
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
-    }
+
 }
