a37d7fd67232b43157b9ad24904700c79b82d88c
Nia <103027252+SFP95@users.noreply.github.com>
2022-11-15 14:05:26 +0100

    clase 11/11/22

--- app/src/main/java/org/niferp/primero/SecundaryActivity.java
+++ app/src/main/java/org/niferp/primero/SecundaryActivity.java
@@ -5,6 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class SecundaryActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
 
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
 
+    }
+    //5.2 Esta en el activity_main2
 }
