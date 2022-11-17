bdf4135d23ecd3be2a8e9556caf96929076213af
Nia <103027252+SFP95@users.noreply.github.com>
2022-11-17 10:11:32 +0100

    clase 17/11/22

--- app/src/main/java/org/niferp/primero/SecundaryActivity.java
+++ app/src/main/java/org/niferp/primero/SecundaryActivity.java
@@ -23,6 +23,8 @@ public class SecundaryActivity extends AppCompatActivity{
         //tv.setText("Hola "+ nombre);
         tv.setText("Hola "+ persona);
 
+
+
         //int edad=getIntent().getIntExtra("Edad", -1);
 
         //Toast.makeText(this,"Hola"+ nombre+ "! \n tu edad es de "+(edad+2),Toast.LENGTH_LONG).show();
