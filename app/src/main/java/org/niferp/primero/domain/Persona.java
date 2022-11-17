760745060c21a8a607f89be98afb1a054b1af501
Nia <103027252+SFP95@users.noreply.github.com>
2022-11-17 09:45:29 +0100

    clase 17/11/22

---
+++ app/src/main/java/org/niferp/primero/domain/Persona.java
@@ -0,0 +1,23 @@
+package org.niferp.primero.domain;
+
+public class Persona {
+    private String nombre;
+    private int edad;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+}
