645915cee9b117a1acaf1f28c469845d6755d75e
alumno <dam.sonia.fernandez@gmail.com>
2022-11-27 13:13:04 +0100

    creacion ej4

---
+++ app/src/main/java/srcNew/Ej4/MainActivity.java
@@ -0,0 +1,17 @@
+package srcNew.Ej4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import org.niferp.primero.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main5);
+        
+    }
+}
