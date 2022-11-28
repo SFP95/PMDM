17b525264c80a2eea7164ceae6ccbb9ab16cb8d2
alumno <dam.sonia.fernandez@gmail.com>
2022-11-28 17:52:23 +0100

    ajustes y añadir calculadora

---
+++ app/src/main/java/srcNew/Ej5/MainActivity.java
@@ -0,0 +1,16 @@
+package srcNew.Ej5;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import org.niferp.primero.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(org.niferp.primero.R.layout.activity_main7);
+    }
+}
