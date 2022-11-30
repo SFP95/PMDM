5c1f122eda5f76ad9a56771e2c404cde0d386290
Nia <103027252+SFP95@users.noreply.github.com>
2022-11-30 08:40:32 +0100

    ej slipt

--- app/src/main/java/srcNew/Split/EjSplit.java
+++ app/src/main/java/srcNew/Split/EjSplit.java
@@ -1,5 +1,12 @@
 package srcNew.Split;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.view.View;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class EjSplit {
     private static String text="Alba-Jessica-Carols-David-Marcelo-Gavi-Fouad-Rafa-Maria";
 
@@ -12,6 +19,36 @@ public class EjSplit {
         for(int i =0;i<palabra.length;i++){
             System.out.println(i+" "+ palabra[i]);
         }
-
     }
+
+    //Forma de aplicarlo en un casi de implicitas
+
+    public void caso2b(View v) {
+        String[] palabra = text.split("-");
+        for (int i = 0; i < palabra.length; i++) {
+            System.out.println(i + " " + palabra[i]);
+        }
+        System.out.println("*************************");
+        List<String> lista = new ArrayList<String>();
+        String cadena = "HOLA-BUENOS-DIAS";
+        String[] palabras = cadena.split("-");
+        for (String el : palabra) {
+            // System.out.println(el);
+            lista.add(el);
+            System.out.println(lista.get(1));
+        }
+    }
+    /**
+     * public void separarcadenas(){
+     *         String cadena= "Hola-cadena";
+     *         String mensaje="";
+     *         cadena.split("-");
+     *         String[] separated =  cadena.split("-");
+     *         for(String palabra: separated){
+     *             System.out.println(palabra);
+     *             mensaje +=palabra+"&&";
+     *         }
+     *
+     *     }
+     */
 }
